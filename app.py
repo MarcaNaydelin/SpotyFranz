531c9b2c203930ac30b5b18363be81e1c579aad8
MarcaNaydelin <naydelingirl1234@gmail.com>
2023-04-28 08:44:36 -0400

    version 28-04_1 nuevoRegistro.html

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
 
+class Cancion:
+    def __init__(self,titulo,categoria,idioma):
+        self.titulo=titulo
+        self.categoria= categoria
+        self.idioma=idioma
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('listar.html', titulo='Canciones')
 
+def index():
+  cancion1=Cancion('La guitarra','pop','castellano')
+  cancion2=Cancion('para no verte mas','pop','castellano')
+  cancion3=Cancion('balada para un gordo','balada','castellano')
+  lista=[cancion1,cancion2,cancion3]    
+    
+  return render_template('listar.html', titulo='Canciones',musicas=lista)
+
+@app.route('/nuevoregistro')
+def nuevoregistro():
+    return render_template('nuevoRegistro.html', titulo='NuevaCancion')
+  
 app.run(host="0.0.0.0", port=5000)
